ec24ab39a604fdcad01161c8b2a93243f231cc5e
rishabh7766 <84180747+rishabh7766@users.noreply.github.com>
2021-05-14 15:51:22 +0530

    Add files via upload

---
+++ numpy_module.py
@@ -0,0 +1,36 @@
+#  import numpy as np
+
+
+'''a = np.array([1, 2, 3, 4, 5], dtype=float)
+print(a)
+
+for i in a:
+    print(type(i))'''
+
+'''a = np.array([[1, 2, 3], [4, 5, 6]])
+print(a.ndim)
+print(a[1, 1])
+b = np.array([[[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]], [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]])
+print(b.ndim, b.shape)
+print( b[1, 0, 1, 2])
+print(b.size, b.nbytes)'''
+'''# A = np.arange(10+)
+# B = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# print(A, B)
+# print(list(range(10)))
+
+C = np.random.permutation(np.arange(10))
+D = np.random.randint(20, 30)
+E = np.random.power(4, 2)
+# print(C, D, E)
+
+# A = np.array([[1, 3, 4], [3, 5, 6], [1, 3, 4], [3, 5, 6], [1, 3, 4], [3, 5, 6]])
+# print(A.reshape(9, 2))
+
+
+F = np.random.rand(1000)
+# print(F)
+G = np.random.rand(3, 2, 1)
+print(G)'''
+
+
